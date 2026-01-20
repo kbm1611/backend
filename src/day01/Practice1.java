cfbeb64433e5a7fc2adaf79ecd4fa29f6ff17f2a
kbm1611 <bm1611@naver.com>
2026-01-20 10:06:08 +0900

    day01 practice1

--- src/day01/Practice1.java
+++ src/day01/Practice1.java
@@ -2,7 +2,7 @@ package day01;
 
 import java.util.Scanner;
 
-public class practice1 {
+public class Practice1 {
     public static void main(String[] args) {
         //문제1
         System.out.println( 25 );
