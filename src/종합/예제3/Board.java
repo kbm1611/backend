0616b0f8210c94089bcdb952dc08149f600a3dd2
kbm1611 <bm1611@naver.com>
2026-01-26 18:01:20 +0900

    day05 오후 종합예제3

---
+++ src/종합/예제3/Board.java
@@ -0,0 +1,6 @@
+package 종합.예제3;
+
+public class Board {
+    String content;
+    String writer;
+}
---
+++ src/종합/예제3/BoardService3.java
@@ -0,0 +1,48 @@
+package 종합.예제3;
+
+import java.util.Scanner;
+
+public class BoardService3 {
+
+    public static void main(String[] args) {
+        Board[] boards = new Board[100];
+        Scanner scan = new Scanner(System.in);
+
+        for( ; ; ){
+            System.out.println("====== My Community ======");
+            System.out.println("1게시물쓰기 2.게시물출력");
+            System.out.println("==========================");
+            System.out.print("선택>");
+
+            int ch = scan.nextInt();
+            //[5] 제어문, 코드 흐름 제어( 논리적인 사고, 경우의 수 등)
+            if( ch == 1 ){
+                Board board = new Board();
+                scan.nextLine(); // .nextLine() 앞에 다른 next 존재하면 하나로 인식
+                System.out.print("내용 : " );   board.content = scan.nextLine();
+                System.out.print("작성자 : ");   board.writer = scan.nextLine();
+
+                for(int index = 0; index <= boards.length-1; index++){
+                    if( boards[index] == null){
+                        boards[index] = board;
+                        System.out.println("[안내] 글쓰기 성공");
+                        System.out.println();
+                        break;
+                    } else{
+                        System.out.println("[경고] 게시물 등록할 공간이 없습니다.");
+                    }
+                }
+            }else if(ch == 2){
+                for(int index = 0; index<= boards.length-1; index++){
+                    if(boards[index] == null){
+                        continue;
+                    }else{
+                        System.out.printf("작성자 : %s\n내용 : %s\n", boards[index].writer, boards[index].content);
+                        System.out.println("---------------------------------------");
+                    }
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
